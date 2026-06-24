5e1a7190ae1207b1ef5a9ed66526547243054551
ankitmehtamp <ankit.mehta@mobileprogramming.com>
2026-06-24 17:23:48 +0530

    Initial commit: WebToApp Android template

---
+++ app/src/main/java/com/example/webtoapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.webtoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.webtoapp.ui.theme.WebToAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WebToAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                 /*   Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                    WebPage(
+                        url = "https://www.google.com"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WebToAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/webtoapp/WebPage.kt
@@ -0,0 +1,20 @@
+package com.example.webtoapp
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WebPage(url: String) {
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                loadUrl(url)
+            }
+        }
+    )
+}
